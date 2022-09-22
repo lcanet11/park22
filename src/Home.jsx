9bb556e438999a07b19267bb5f6f4864a459ba11
Luci <Luci.Canet@gmail.com>
2022-09-22 13:14:39 -0400

    add signup form to Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,67 @@
 import React, {Component} from 'react'; 
 
+const registrarse = () => {
+    var nombre;
+    nombre = document.form_registro.nombre.value; 
+    var apellido = document.form_registro.apellido.value; 
+    var email = document.getElementById("email_id").value; 
+    var telefono = document.getElementById("tel_id").value;
+    var password = document.form_registro.password.value; 
+    var confirmar_password=document.form_registro.confirmar_password.value; 
+    console.log(nombre, apellido, email, password, confirmar_password);
+    if(nombre=="") {
+        document.getElementById("nombre_error").innerHTML = "El campo nombre es obligatorio"
+    } else {
+        document.getElementById("nombre_error").innerHTML = ""
+    }
+}
+
+const proveedor = () => {
+    var proveedor = document.getElementById("proveedor_id").value;
+    console.log(proveedor);
+
+    var servicio; 
+    switch(proveedor) {
+        case "1": servicio="Claro";
+        break;
+        case "2": servicio="Personal"
+        break;
+        case "3": servicio="Movistar"
+        break;
+        case "4": servicio="Tuenti"
+        break;
+    }
+
+}
+
 class Home extends Component{
     render(){
         return(
-            <>
-            <div class="field">
+            <form name="form_registro">
+            <div className="field">
             <label>Nombre</label>
             <input type="text" name="nombre" id="nombre_id"></input>
             </div>
+            <div id="nombre_error"></div>
 
-            <div class="field">
+            <div className="field">
             <label>Apellido</label>
             <input type="text" name="apellido" id="apellido_id"></input>
             </div>
 
-            <div class="field">
+            <div className="field">
             <label>Email</label>
             <input type="text" name="email" id="email_id"></input>
             </div>
 
-            <div class="field">
-            <label>Telephone</label>
+            <div className="field">
+            <label>Telefono</label>
             <input type="text" name="tel" id="tel_id"></input>
             </div>
 
             <div>
             <label>Proveedor</label>
-            <select name="proveedor" id="proveedor_id" onchange="Proveedor()">
+            <select name="proveedor" id="proveedor_id" onChange={proveedor}>
                 <option value="1">Claro</option>
                 <option value="2">Personal</option>
                 <option value="3">Movistar</option>
@@ -34,16 +69,17 @@ class Home extends Component{
             </select>
             </div>
 
-            <div class="field">
+            <div className="field">
             <label>Password</label>
             <input type="text" name="password" id="password_id"></input>
             </div>
 
-            <div class="field">
+            <div className="field">
             <label>Confirmar Password</label>
-            <input type="text" name="password" id="password_id"></input>
+            <input type="text" name="confirmar_password" id="confirmar_password_id"></input>
             </div>
-            </>
+            <button onSubmit={registrarse}>Registrarse</button>
+            </form>
         )
     }
 }
